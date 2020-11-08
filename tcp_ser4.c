be6f1a2089bbffc235394b073acc8f7792b0cd6c
shenghaoc <chen.shenghao@u.nus.edu>
2020-11-09 00:20:43 +0800

    Add current progress

--- tcp_ser4.c
+++ tcp_ser4.c
@@ -2,111 +2,135 @@
 tcp_ser.c: the source file of the server in tcp transmission 
 ***********************************/
 
-
 #include "headsock.h"
 
 #define BACKLOG 10
 
-void str_ser(int sockfd);                                                        // transmitting and receiving function
+void str_ser(int sockfd, double error_p); // transmitting and receiving function
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int sockfd, con_fd, ret;
 	struct sockaddr_in my_addr;
 	struct sockaddr_in their_addr;
 	int sin_size;
+	struct timeval seedt;
+	double error_p;
 
-//	char *buf;
 	pid_t pid;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);          //create socket
-	if (sockfd <0)
+	if (argc != 2)
+	{
+		printf("parameters not match");
+	}
+
+	error_p = atof(argv[1]);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); //create socket
+	if (sockfd < 0)
 	{
 		printf("error in socket!");
 		exit(1);
 	}
-	
+
 	my_addr.sin_family = AF_INET;
 	my_addr.sin_port = htons(MYTCP_PORT);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr("172.0.0.1");
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); //inet_addr("172.0.0.1");
 	bzero(&(my_addr.sin_zero), 8);
-	ret = bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr));                //bind socket
-	if (ret <0)
+	ret = bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)); //bind socket
+	if (ret < 0)
 	{
 		printf("error in binding");
 		exit(1);
 	}
-	
-	ret = listen(sockfd, BACKLOG);                              //listen
-	if (ret <0) {
+
+	ret = listen(sockfd, BACKLOG); //listen
+	if (ret < 0)
+	{
 		printf("error in listening");
 		exit(1);
 	}
 
+    gettimeofday(&seedt, NULL);
+    srand(seedt.tv_usec * seedt.tv_sec);
+
 	while (1)
 	{
 		printf("waiting for data\n");
-		sin_size = sizeof (struct sockaddr_in);
-		con_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);            //accept the packet
-		if (con_fd <0)
+		sin_size = sizeof(struct sockaddr_in);
+		con_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size); //accept the packet
+		if (con_fd < 0)
 		{
 			printf("error in accept\n");
 			exit(1);
 		}
 
-		if ((pid = fork())==0)                                         // creat acception process
+		if ((pid = fork()) == 0) // creat acception process
 		{
 			close(sockfd);
-			str_ser(con_fd);                                          //receive packet and response
+			str_ser(con_fd, error_p); //receive packet and response
 			close(con_fd);
 			exit(0);
 		}
-		else close(con_fd);                                         //parent process
+		else
+			close(con_fd); //parent process
 	}
 	close(sockfd);
 	exit(0);
 }
 
-void str_ser(int sockfd)
+void str_ser(int sockfd, double error_p)
 {
 	char buf[BUFSIZE];
 	FILE *fp;
 	char recvs[DATALEN];
 	struct ack_so ack;
-	int end, n = 0;
-	long lseek=0;
+	int seq_num = 0;
+	int end;
+	int n = 0;
+	long lseek = 0;
 	end = 0;
-	
+	int has_err = 0;
+
 	printf("receiving data!\n");
 
-	while(!end)
+	while (!end)
 	{
-		if ((n= recv(sockfd, &recvs, DATALEN, 0))==-1)                                   //receive the packet
+		if (error_p > (double) rand() / RAND_MAX)
+		{
+			has_err = 1;
+		}
+		else
+		{
+			has_err = 0;
+		}
+		
+		if ((n = recv(sockfd, &recvs, DATALEN, 0)) == -1) //receive the packet
 		{
 			printf("error when receiving\n");
 			exit(1);
 		}
-		if (recvs[n-1] == '\0')									//if it is the end of the file
+		if (recvs[n - 1] == '\0') //if it is the end of the file
 		{
 			end = 1;
-			n --;
+			n--;
 		}
-		memcpy((buf+lseek), recvs, n);
+		memcpy((buf + lseek), recvs, n);
 		lseek += n;
 	}
 	ack.num = 1;
 	ack.len = 0;
-	if ((n = send(sockfd, &ack, 2, 0))==-1)
+	if ((n = send(sockfd, &ack, 2, 0)) == -1)
 	{
-			printf("send error!");								//send the ack
-			exit(1);
+		printf("send error!"); //send the ack
+		exit(1);
 	}
-	if ((fp = fopen ("myTCPreceive.txt","wt")) == NULL)
+	if ((fp = fopen("myTCPreceive.txt", "wt")) == NULL)
 	{
-		printf("File doesn't exit\n");
+		printf("File doesn't exist\n");
 		exit(0);
 	}
-	fwrite (buf , 1 , lseek , fp);					//write data into file
+	fwrite(buf, 1, lseek, fp); //write data into file
 	fclose(fp);
 	printf("a file has been successfully received!\nthe total data received is %d bytes\n", (int)lseek);
 }

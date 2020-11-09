ade7f895f505727e8aebf5b3b27a19dd5d042833
shenghaoc <chen.shenghao@u.nus.edu>
2020-11-10 01:52:28 +0800

    Update shell scripts

--- dus_multi.sh
+++ dus_multi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-x=235; while [ $x -le 50000 ]; do echo $x; bash dus_single.sh $x >> dus.txt; (( x = x + 500 )) ;  done
+rm dus.txt
+x=235; while [ $x -le 50000 ]; do echo $x; bash dus_single.sh $1 $x >> dus.txt; sleep 0.01; (( x = x + 500 )) ;  done
 
--- dus_single.sh
+++ dus_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./tcp_ser4 0 $1 & 
-./tcp_client4 127.0.0.1 $1 & 
+./tcp_ser4 $1 $2 & 
+./tcp_client4 127.0.0.1 $2 & 
 wait -n 
 pkill -P $$ 
 

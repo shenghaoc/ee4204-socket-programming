7b85b602555183de1d5d957882118f3188ef7136
shenghaoc <shenghaoc@outlook.com>
2021-11-14 22:23:07 +0800

    Replace semicolon with newline

--- dus_multi.sh
+++ dus_multi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 rm dus.txt
-x=235; while [ $x -le 50000 ]; do echo $x; bash dus_single.sh $1 $x >> dus.txt; sleep 0.01; (( x = x + 500 )) ;  done
+x=235
+while [ $x -le 50000 ]
+do echo $x
+    bash dus_single.sh $1 $x >> dus.txt
+    sleep 0.01
+    (( x = x + 500 )) 
+done
 
